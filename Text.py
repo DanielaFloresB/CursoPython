3582118dab68860ba5fbd9c5604627a542be7e28
DanielaF <danyy.flo@gmail.com>
2018-01-08 11:35:42 -0600

    Prueba hola mundo

---
+++ Text.py
@@ -0,0 +1,36 @@
+# Coding archivo para que detecte los carácteres y el tipo de intérprete
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#-------------------------------------
+#Header
+__author__ = "Daniela Flores"
+__copyright__ = "Copyright 2007, ST Project"
+__credits__ = ["Daniela Flores"]
+__license__ = "GPL 2"
+__version__ = "1.0.1"
+__maintainer__ = "Dan"
+__email__ = "hola@python.com"
+__status__ =  "Debbug"
+__execution__ = "python3 "
+
+#-------------------------------------
+#Library
+#import <lib>
+import random  
+
+#-------------------------------------
+#Class 
+#	def():
+
+def myfirstHello():
+	#Python 2.7     -> print "Hola Mundo"   
+	print("HolaMundo")		#Python 3.0
+
+
+#-------------------------------------
+#
+if __name__ == "__main__":
+    myfirstHello()
+
+
+#-------------------------------------
